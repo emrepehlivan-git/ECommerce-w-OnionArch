5db2f3f57f908a2b5086d7febe7f22cbc0c30b92
emrepehlivan-git <emrepehli@gmail.com>
2025-01-09 20:08:58 +0300

    added base entity class and auditable entity interface

---
+++ src/Core/ECommerce.Domain/Common/BaseEntity.cs
@@ -0,0 +1,6 @@
+namespace ECommerce.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+}
---
+++ src/Core/ECommerce.Domain/Common/IAuditableEntity.cs
@@ -0,0 +1,9 @@
+namespace ECommerce.Domain.Common;
+
+public interface IAuditableEntity
+{
+    DateTime CreatedAt { get; set; }
+    string? CreatedBy { get; set; }
+    DateTime? LastModifiedAt { get; set; }
+    string? LastModifiedBy { get; set; }
+}
